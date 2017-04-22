02075e16265eb110d1d775b3f7b30480941836df
Cybertek Student <CybertekStudent@WIN-GEDESCREUP8.localdomain>
2017-04-22 17:36:01 -0400

    12123123

--- src/com/cbt/AppTest.java
+++ src/com/cbt/AppTest.java
@@ -5,7 +5,7 @@ public class AppTest {
 	public static void main(String[] args) {
 		//comment comment
 		System.out.println("it is work");
-		System.out.println();
+		System.out.println("werwerwer");
 		System.out.println();
 	}
 
